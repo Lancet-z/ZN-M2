f8e86542bc612dc637ab6e9871dda1499cdebbf2
Lancet-z <55058538+Lancet-z@users.noreply.github.com>
2022-06-03 19:55:06 +0800

    Create part1.sh

---
+++ part1.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#新的argon主题
+rm -rf ./package/lean/luci-theme-argon
+#rm -rf ./package/lean/luci-app-jd-dailybonus
+# Add a feed source
+#sed -i 's/PATCHVER:=5.10/PATCHVER:=5.4/g' target/linux/x86/Makefile
+#sed -i '$a src-git lienol https://github.com/kenzok8/openwrt-packages.git' feeds.conf.default
+# 科学插件
+#echo 'src-git helloworld https://github.com/fw876/helloworld.git' >>feeds.conf.default
+#echo 'src-git openclash https://github.com/vernesong/OpenClash.git' >>feeds.conf.default
+
+#新版passwall2
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git' >>feeds.conf.default
+echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git' >>feeds.conf.default
