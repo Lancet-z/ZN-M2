7a0c55c7ddbce26cabe54adc3d9af88ed60bfa7f
Lancet-z <55058538+Lancet-z@users.noreply.github.com>
2022-06-10 01:00:53 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,8 +10,8 @@ rm -rf ./package/lean/luci-theme-argon
 #echo 'src-git openclash https://github.com/vernesong/OpenClash.git' >>feeds.conf.default
 
 #新版passwall2
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git' >>feeds.conf.default
-echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git' >>feeds.conf.default
+#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git' >>feeds.conf.default
+#echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git' >>feeds.conf.default
 
 #openclash
 echo 'src-git openclash https://github.com/vernesong/OpenClash.git' >>feeds.conf.default
