923a26511b71088554bc7465c6fd2cad1cf12332
Lancet-z <55058538+Lancet-z@users.noreply.github.com>
2022-06-12 16:23:51 +0800

    Update part1.sh

--- part1.sh
+++ part1.sh
@@ -12,3 +12,8 @@ rm -rf ./package/lean/luci-theme-argon
 #新版passwall2
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git' >>feeds.conf.default
 echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git' >>feeds.conf.default
+echo 'src-git Rinze https://github.com/Lancet-z/luci-theme-Rinze.git' >>feeds.conf.default
+#添加adghomme
+pushd package/lean
+git clone https://github.com/kongfl888/luci-app-adguardhome.git
+popd
