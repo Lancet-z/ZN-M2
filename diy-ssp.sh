433e37dbecd752e4cee29b2fe55696f0e56c3e27
Lancet-z <55058538+Lancet-z@users.noreply.github.com>
2022-06-16 18:51:29 +0800

    Update diy-ssp.sh

--- diy-ssp.sh
+++ diy-ssp.sh
@@ -1,11 +1,12 @@
+ echo 'src-git small8 https://github.com/kenzok8/small-package.git' >>feeds.conf.default
 
 # 科学插件
-echo 'src-git helloworld https://github.com/fw876/helloworld.git' >>feeds.conf.default
+#echo 'src-git helloworld https://github.com/fw876/helloworld.git' >>feeds.conf.default
 
 
 #新版passwall2
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git' >>feeds.conf.default
-echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git' >>feeds.conf.default
+#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git' >>feeds.conf.default
+#echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git' >>feeds.conf.default
 
 #openclash
-echo 'src-git openclash https://github.com/vernesong/OpenClash.git' >>feeds.conf.default
+#echo 'src-git openclash https://github.com/vernesong/OpenClash.git' >>feeds.conf.default
