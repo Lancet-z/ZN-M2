37402fbac4d1ea6a226950ef7d348b820b01b446
Lancet-z <55058538+Lancet-z@users.noreply.github.com>
2022-06-17 02:05:38 +0800

    Update diy-ssp.sh

--- diy-ssp.sh
+++ diy-ssp.sh
@@ -6,7 +6,7 @@ echo 'src-git Rinze https://github.com/Lancet-z/luci-theme-Rinze.git' >>feeds.co
 
 
 #新版passwall2
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git' >>feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git' >>feeds.conf.default
 #echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git' >>feeds.conf.default
 
 #openclash
