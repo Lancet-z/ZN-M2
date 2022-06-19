51356e1b3531638cca35b50957843babc444cb9e
Lancet-z <55058538+Lancet-z@users.noreply.github.com>
2022-06-19 10:32:35 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,2 +1,1 @@
-echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages.git' >>feeds.conf.default
-echo 'src-git Rinze https://github.com/Lancet-z/luci-theme-Rinze.git' >>feeds.conf.default
+
