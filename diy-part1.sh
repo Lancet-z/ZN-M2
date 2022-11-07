0d777a51da090c3ca8fc6a2b890151b00beb585b
Lancet-z <55058538+Lancet-z@users.noreply.github.com>
2022-11-07 20:49:37 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -4,3 +4,6 @@ rm -rf ./package/lean/luci-theme-argon
 echo 'src-git openclash https://github.com/vernesong/OpenClash.git' >>feeds.conf.default
 #主题列表
 echo 'src-git Rinze https://github.com/Lancet-z/luci-theme-Rinze.git' >>feeds.conf.default
+# UU加速器
+svn export https://github.com/coolsnowwolf/luci/trunk/applications/luci-app-uugamebooster package/lean/luci-app-uugamebooster
+svn export https://github.com/coolsnowwolf/packages/trunk/net/uugamebooster package/lean/uugamebooster
