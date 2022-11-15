da6feb2ca67f657367dae62b088f65fcf70a8516
Lancet-z <55058538+Lancet-z@users.noreply.github.com>
2022-11-16 02:47:04 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -3,5 +3,5 @@ rm -rf ./package/lean/luci-theme-argon
 #openclash
 echo 'src-git openclash https://github.com/vernesong/OpenClash.git' >>feeds.conf.default
 #主题列表
-echo 'src-git Rinze https://github.com/Lancet-z/luci-theme-Rinze.git' >>feeds.conf.default
+#echo 'src-git Rinze https://github.com/Lancet-z/luci-theme-Rinze.git' >>feeds.conf.default
 
