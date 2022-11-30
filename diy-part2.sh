064b2b202df44fcf058f368023911f11f3b10996
Lancet-z <55058538+Lancet-z@users.noreply.github.com>
2022-11-30 23:46:56 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -15,6 +15,7 @@ sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generat
 
 # 修改 argon 为默认主题,可根据你喜欢的修改成其他的（不选择那些会自动改变为默认主题的主题才有效果）
 git clone https://github.com/jerrykuku/luci-theme-argon.git feeds/luci/themes/luci-theme-argon
+git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
 
 # 修改主机名字，把OpenWrt-123修改你喜欢的就行（不能使用中文）
 #sed -i '/uci commit system/i\uci set system.@system[0].hostname='Lancet'' package/lean/default-settings/files/zzz-default-settings
